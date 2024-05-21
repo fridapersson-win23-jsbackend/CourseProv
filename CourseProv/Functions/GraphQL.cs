35b89b6f4515ed4232ffb34d9c5aeac748cc2552
Frida <fridapeersson@gmail.com>
2024-05-21 20:52:37 +0200

    idk

--- CourseProv/Functions/GraphQL.cs
+++ CourseProv/Functions/GraphQL.cs
@@ -1,19 +1,26 @@
-﻿using HotChocolate.AzureFunctions;
+using HotChocolate.AzureFunctions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace CourseProvider.Functions;
-
-public class GraphQL(ILogger<GraphQL> logger, IGraphQLRequestExecutor graphQLRequestExecutor)
+namespace CourseProv.Functions
 {
-    private readonly ILogger<GraphQL> _logger = logger;
-    private readonly IGraphQLRequestExecutor _graphQLRequestExecutor = graphQLRequestExecutor;
-
-    [Function("GraphQL")]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "graphql")] HttpRequest req)
+    public class GraphQL
     {
-        return await _graphQLRequestExecutor.ExecuteAsync(req);
+        private readonly ILogger<GraphQL> _logger;
+        private readonly IGraphQLRequestExecutor _graphQLRequestExecutor;
+
+        public GraphQL(ILogger<GraphQL> logger, IGraphQLRequestExecutor graphQLRequestExecutor)
+        {
+            _logger = logger;
+            _graphQLRequestExecutor = graphQLRequestExecutor;
+        }
+
+        [Function("GraphQL")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route ="graphql")] HttpRequest req)
+        {
+            return await _graphQLRequestExecutor.ExecuteAsync(req);
+        }
     }
 }
