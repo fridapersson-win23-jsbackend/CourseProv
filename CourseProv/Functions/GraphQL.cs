cd22bf278cb32845945760a644bbd304fd4a2fbd
Frida <fridapeersson@gmail.com>
2024-05-22 12:18:29 +0200

    .

--- CourseProv/Functions/GraphQL.cs
+++ CourseProv/Functions/GraphQL.cs
@@ -1,24 +1,19 @@
+using HotChocolate.AzureFunctions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace CourseProv.Functions
+namespace CourseProv.Functions;
+
+public class GraphQL(ILogger<GraphQL> logger, IGraphQLRequestExecutor graphQLRequestExecutor)
 {
-    public class GraphQL
+    private readonly ILogger<GraphQL> _logger = logger;
+    private readonly IGraphQLRequestExecutor _graphQLRequestExecutor = graphQLRequestExecutor;
+
+    [Function("GraphQL")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "graphql")] HttpRequest req)
     {
-        private readonly ILogger<GraphQL> _logger;
-
-        public GraphQL(ILogger<GraphQL> logger)
-        {
-            _logger = logger;
-        }
-
-        [Function("GraphQL")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "graphql")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
-        }
+        return await _graphQLRequestExecutor.ExecuteAsync(req);
     }
 }
